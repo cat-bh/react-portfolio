b26f48f483b7ac4f7af47bd2a4c210eb1ab495dd
Catherine <catheneghan@gmail.com>
2021-11-21 00:56:16 -0800

    set up app.js to return the header, footer, and main content based on a switch statement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+//Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import About from './components/About';
+import Contact from './components/Contact';
+import Work from './components/Work';
+import Resume from './components/Resume';
 
 function App() {
+  const [categories] = useState(['about', 'contact', 'work', 'resume']);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const renderPage = () => {
+    // Add a switch statement that will return the appropriate component of the 'currentPage'
+    // YOUR CODE HERE
+    switch(currentCategory) {
+      case 'about': return <About/>;
+      case 'contact': return <Contact/>;
+      case 'work': return <Work/>;
+      case 'resume': return <Resume/>;
+      default: return <About/>;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Header>
+      <main>
+        {renderPage()}
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
