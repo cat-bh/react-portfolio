7b7be804040c1af9ebb9c9161c47cc3204c8d94f
Catherine <catheneghan@gmail.com>
2021-11-21 19:06:32 -0800

    added links in the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,8 +3,9 @@ import React from "react";
 function Footer() {
     return (
         <footer>
-            This is the footer
-            Links to social media go here
+            <a href="https://github.com/cat-bh" target="_blank" rel='noreferrer noopener'>GitHub</a> | 
+            <a href="https://www.linkedin.com/in/catherine-heneghan-519634211/" target="_blank" rel='noreferrer noopener'> LinkedIn</a> | 
+            <a href="https://twitter.com" target="_blank" rel='noreferrer noopener'> Twitter</a>
         </footer>
     )
 
